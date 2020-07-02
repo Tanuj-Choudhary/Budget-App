cd9cd09ff0f8cdd3e4291eccf678912b03e89dbb
Tanuj <tanujchoudhary001@gmail.com>
2020-07-02 15:05:53 +0530

    Adding addItem to Model

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,17 +1,59 @@
 //MODEL
 var budgetController = (function () {
 
-	var Expense = function(id,description,value) {
-		this.id = id;
-		this.description = description;
-		this.value = value;
-	}
+    var Expense = function (id, description, value) {
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    };
 
-	var Income = function(id,description,value) {
-		this.id = id;
-		this.description = description;
-		this.value = value;
-	}
+    var Income = function (id, description, value) {
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    };
+
+    var data = {
+        allItems: {
+            exp: [],
+            inc: []
+        },
+
+        totals: {
+            exp: 0,
+            inc: 0
+        }
+    };
+
+    return {
+        addItem: function (type, desc, val) {
+            var newItem;
+
+            //Get length of exp or inc array
+            var length = data.allItems[type].length;
+            var ID;
+
+
+            //Retrieve the ID of last element and add 1 to create new ID
+            if (data.allItems[type].length > 0) {
+                ID = data.allItems[type][length - 1].id + 1;
+            } else {
+                ID = 0;
+            }
+
+            //Create new item based on 'inc' or 'exp' type
+            if (type === 'exp') {
+                newItem = new Expense(ID, desc, val);
+            } else if (type === 'inc') {
+                newItem = new Income(ID, desc, val);
+            }
+
+            //adding according to type of data
+            data.allItems[type].push(newItem);
+
+            return newItem;
+        }
+    };
 
 })();
 
@@ -63,14 +105,14 @@ var controller = (function (budgetCtrl, UICtrl) {
             }
         });
 
-    }
+    };
 
     var ctrlAdditem = function () {
 
         //Get the input data
         var input = UIController.getInput();
         //Add item to Budget Controller
-
+        var newItem = budgetController.addItem(input.type, input.desc, input.value);
         //Add the item to UI
 
         //Calculate the Budget
@@ -79,10 +121,10 @@ var controller = (function (budgetCtrl, UICtrl) {
     };
 
     return {
-    	init: function() {
-    		setupEventListeners();
-    	}
-    }
+        init: function () {
+            setupEventListeners();
+        }
+    };
 
 })(budgetController, UIController);
 
