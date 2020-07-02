995a10646349406d173a28660b88c2cc894e014a
Tanuj <tanujchoudhary001@gmail.com>
2020-07-02 12:35:54 +0530

    Add module template to js

---
+++ resources/js/app.js
@@ -0,0 +1,26 @@
+//MODEL
+var budgetController = (function() {
+
+	
+})();
+
+
+
+
+
+//VIEW
+var  UIController = (function() {
+
+
+})();
+
+
+
+
+
+
+//CONTROLLER
+var controller = (function(budgetCtrl,UICtrl) {
+
+
+})(budgetController,UIController);
